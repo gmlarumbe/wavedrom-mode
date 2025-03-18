249aec11fdab12ae6228c2bc2580b581d16e3443
Gonzalo Larumbe <gonzalomlarumbe@gmail.com>
2025-03-18 14:49:58 +0100

    Update copyright year

--- wavedrom-mode.el
+++ wavedrom-mode.el
@@ -1,6 +1,6 @@
 ;;; wavedrom-mode.el --- WaveDrom Integration  -*- lexical-binding: t -*-
 
-;; Copyright (C) 2023-2024 Gonzalo Larumbe
+;; Copyright (C) 2022-2025 Gonzalo Larumbe
 
 ;; Author: Gonzalo Larumbe <gonzalomlarumbe@gmail.com>
 ;; URL: https://github.com/gmlarumbe/wavedrom-mode
